826cf6e406489e640805cc629482fc929e36d05c
leemj0948 <ljo094822@naver.com>
2023-05-30 13:33:42 +0900

    add: res html에 노출

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+interface responseType{
+  userId: number,
+    id: number,
+    title: string,
+    body:string
+}
 function App() {
   const [count, setCount] = useState(0)
-  const [res,setRest] = useState<string[]>([])
+  const [res,setRest] = useState<responseType[]>([])
   const callApi = ()=>{
     const URL = 'https://jsonplaceholder.typicode.com/posts'
     fetch(URL)
     .then(response => response.json())
-    .then((data:string) => {
-      const resObj:string[] = [...res];
-      resObj.push(data ? data : '');
+    .then((data:responseType) => {
+      const resObj:responseType[] = [...res];
+      data??resObj.push(data);
       setRest(resObj);
     })
     .catch(error => console.error(error));
@@ -36,9 +41,16 @@ function App() {
         <button onClick={btnClick}>
           count is {count}
         </button>
+        {res.map((list:responseType,i:number)=>{
+          return (
+            <p key={i}>{list.title}</p>
+          )
+          
+        })}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
